6138672efb9377cb1ffe1a09e5b15b1b300bfd5b
RogersMLCH <mneftali10@outlook.com>
2024-09-02 14:19:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_roger_maldonado
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_roger_maldonado/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_roger_maldonado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
