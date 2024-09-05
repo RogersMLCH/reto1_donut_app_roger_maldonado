0790867ddee650760408028b647c693b01a44fee
RogersMLCH <mneftali10@outlook.com>
2024-09-05 15:17:30 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,33 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size:36 ), onPressed: () {print("mensaje1");},),
+        ),
+        actions:  [Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: IconButton(icon: Icon(Icons.person, color: Colors.grey[800], size:36), onPressed: () {print("mensaje2");},),
+        )],
+
+
       ),
+      body: const Column(children: [
+        //texto
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text("I want to ",style: TextStyle(fontSize: 32),),
+              Text("Eat",style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
+
+            ],
+          ),
+        )
+        //Tab bar
+
+        //Tab bar view 
+      ],),
     );
   }
 }
